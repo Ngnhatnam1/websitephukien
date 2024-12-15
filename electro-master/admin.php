df940c69e7d83ab30eb650e8ad880721e50a0c56
Ngnhatnam1 <jahssinandjashper@gmail.com>
2024-12-16 00:58:04 +0700

    sửa admin

--- electro-master/admin.php
+++ electro-master/admin.php
@@ -13,67 +13,296 @@ $user_result = $conn->query($user_sql);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giao diện Admin</title>
-    <style>
-         body{
-            background-color: #FFCCE1;
-         }
-    </style>
-    <!-- Thư viện Swiper, dùng cho các tính năng dạng trượt cho mượt hơn (áp dụng cho CSS và JS)-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <!-- dùng font awesome thông qua link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/js.js" defer></script>
-</head>
-<body>
-    <div class="wrapper">
-        <!-- Thanh sidebar -->
-        <div class="sidebar" id="sidebar">
-            <button class="close-btn" onclick="toggleSidebar()">X</button>
-            <h3>Quản lý</h3>
-            <ul>
-                <li><a href="#" id="tour-list-link" onclick=""></a></li>
-                    <!-- <div id="tour-info" style="display: none;"> -->
-                    <!-- Bảng thông tin tour sẽ được chèn vào đây khi dữ liệu được tải về -->
-                    
-                
-                <!-- Tạo dropdown cho tài khoản đã tạo -->
-                <li class="dropdown">
-                    <a href="#">Tài khoản đã tạo</a>
-                    <div class="dropdown-content">
-                        <a href="#" onclick="filterAccounts('admin')">Admin</a>
-                        <a href="#" onclick="filterAccounts('user')">User</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
+		<title>Electro - HTML Ecommerce Template</title>
 
-        <!-- Nội dung chính -->
-        <div class="content">
-            <button class="toggle-btn" onclick="toggleSidebar()">☰</button>
-            <h1>Giao diện Admin</h1>
-            
-            <!-- Hiển thị thông tin tài khoản -->
-            <div id="account-details" style="display:none;">
-                <h4>Danh sách Tài Khoản:</h4>
-                <div id="account-info">
-                    <!-- Dữ liệu tài khoản sẽ được hiển thị ở đây -->
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+		<!-- Google font -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 
+		<!-- Bootstrap -->
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
 
-<!-- Nút Log out -->
-<a href="login.php">
-    <button class="logout-btn">Log out</button>
-</a>
+		<!-- Slick -->
+		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+		<!-- Font Awesome Icon -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+		<!-- Custom stlylesheet -->
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+    </head>
+	<body>
+		
+		<?php
+
+			include('header.php');
+			include('navigation.php');
+			
+		?>
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">sản phẩm mới</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab1">tay nghe</a></li>
+									<li><a data-toggle="tab" href="#tab1">bàn phím</a></li>
+									<li><a data-toggle="tab" href="#tab1">Cameras</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1">
+										<!-- product -->
+										<?php
+											$sql = "SELECT sanpham.tensanpham,sanpham.gia,sanpham.linkanh,Category.tenloai FROM sanpham JOIN new ON sanpham.masp = new.masp
+												JOIN Category ON sanpham.maloai =Category.maloai
+													";
+											$result = $conn->query($sql);
+											if ($result->num_rows > 0) {
+												while ($row = $result->fetch_assoc()) {
+													echo '<div class="product">
+															<div class="product-img">
+																<img src="' . htmlspecialchars($row['linkanh']) . '" alt="' . htmlspecialchars($row['tensanpham']) . '">
+																<div class="product-label">
+																	<!-- Vị trí ghi giảm giá (nếu có) -->
+																</div>
+															</div>
+															<div class="product-body">
+																<p class="product-category">' . htmlspecialchars($row['tenloai']) . '</p>
+																<h3 class="product-name"><a href="#">' . htmlspecialchars($row['tensanpham']) . '</a></h3>
+																<h4 class="product-price">$' . number_format($row['gia'], 2) . '</h4>
+																<div class="product-rating">
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																</div>
+																<div class="product-btns">
+																	<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+																	<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+																	<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+																</div>
+															</div>
+															<div class="add-to-cart">
+																<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+															</div>
+														</div>';
+												}
+											} else {
+												echo "Không có sản phẩm nào!";
+											}
+											
+											// Đóng kết nối
+											
+										?>						
+										<!-- /product -->
+
+										
+
+										
+									</div>
+									<div id="slick-nav-1" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- HOT DEAL SECTION -->
+		<div id="hot-deal" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<ul class="hot-deal-countdown">
+								<li>
+									<div>
+										<h3>02</h3>
+										<span>Days</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>10</h3>
+										<span>Hours</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>34</h3>
+										<span>Mins</span>
+									</div>
+								</li>
+								<li>
+									<div>
+										<h3>60</h3>
+										<span>Secs</span>
+									</div>
+								</li>
+							</ul>
+							<h2 class="text-uppercase">hot deal this week</h2>
+							<p>New Collection Up to 50% OFF</p>
+							<a class="primary-btn cta-btn" href="#">Shop now</a>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /HOT DEAL SECTION -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">Top selling</h3>
+							<div class="section-nav">
+								<ul class="section-tab-nav tab-nav">
+									<li class="active"><a data-toggle="tab" href="#tab2">tay nghe </a></li>
+									<li><a data-toggle="tab" href="#tab2">bàn phím</a></li>
+									<li><a data-toggle="tab" href="#tab2">Cameras</a></li>
+									<li><a data-toggle="tab" href="#tab2">chuột</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab2" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-2">
+
+										<!-- product -->
+										<?php
+											
+											$sql = "SELECT sanpham.tensanpham,sanpham.gia,sanpham.linkanh,Category.tenloai FROM sanpham JOIN topcelling ON sanpham.masp = topcelling.masp
+												JOIN Category ON sanpham.maloai =Category.maloai";
+											$result = $conn->query($sql);
+											if ($result->num_rows > 0) {
+												while ($row = $result->fetch_assoc()) {
+													echo '<div class="product">
+															<div class="product-img">
+																<img src="' . htmlspecialchars($row['linkanh']) . '" alt="">
+																<div class="product-label">
+																	<!-- Vị trí ghi giảm giá (nếu có) -->
+																</div>
+															</div>
+															<div class="product-body">
+																<p class="product-category">' . htmlspecialchars($row['tenloai']) . '</p>
+																<h3 class="product-name"><a href="product.php">' . htmlspecialchars($row['tensanpham']) . '</a></h3>
+																<h4 class="product-price">$' . number_format($row['gia'], 2) . '</h4>
+																<div class="product-rating">
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																	<i class="fa fa-star"></i>
+																</div>
+																<div class="product-btns">
+																	<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
+																	<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
+																	<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+																</div>
+															</div>
+															<div class="add-to-cart">
+																<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+															</div>
+														</div>';
+												}
+											} else {
+												echo "Không có sản phẩm nào!";
+											}
+											 
+											// Đóng kết nối
+											
+										?>						
+										<!-- /product -->
+
+									</div>
+									<div id="slick-nav-2" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- /Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		
+		<?php
+		include('footer.php')
+		?>
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
 </html>
 
 <?php
